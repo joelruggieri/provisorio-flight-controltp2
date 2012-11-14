b36b6e307cbb9c0be07e3bf9b745389c7d28206c
gvallerino <gvallerino@605bfb04-7f76-4739-ac2b-c6aeacdc3e22>
2012-11-14 07:03:11 +0000

    Volvi a modifcar el objeto volador una vez mas

--- trunk/src/fiuba/algo3/flightcontrol/ObjetoVolador.java
+++ trunk/src/fiuba/algo3/flightcontrol/ObjetoVolador.java
@@ -82,30 +82,30 @@ public abstract class ObjetoVolador {
 		
 	}
 		
-	public void moverse(){
-		/* Mueve al avion siguiendo su trayectoria */
-		
-		Posicion siguientePosicion;
-		boolean tocaUnBorde;
-		
-		siguientePosicion = this.trayectoria.getProximaPosicion(this.posicionActual);
-		
-		for (int i=0; i < this.velocidad; i++){
-						
-			this.actualizarDireccion (this.trayectoria.getVectorDirector(posicionActual));
-			
-			//Validar bordes.
-			tocaUnBorde = this.validarBordes (siguientePosicion);
-			
-			if (tocaUnBorde){
-				this.invertirTrayectoria(siguientePosicion);
-			}
-			
-			this.posicionActual = siguientePosicion;
-			
-		}
-		
-	}
+	 public void moverse(){
+		 /* Mueve al avion siguiendo su trayectoria */
+		 
+		 Posicion siguientePosicion;
+		 boolean tocaUnBorde;
+		 
+		 siguientePosicion = this.trayectoria.getProximaPosicion(this.posicionActual);
+		 
+		 for (int i=0; i < this.velocidad; i++){
+		                                 
+		         this.actualizarDireccion (this.trayectoria.getVectorDirector(posicionActual));
+		         
+		         //Validar bordes.
+		         tocaUnBorde = this.validarBordes (siguientePosicion);
+		         
+		         if (tocaUnBorde){
+		                 this.invertirTrayectoria(siguientePosicion);
+		         }
+		         
+		         this.posicionActual = siguientePosicion;
+		         
+		 }
+         
+	 }
 	
 	public boolean aterrizo(){
 		/* Devuelve si el avion aterrizo o no */
