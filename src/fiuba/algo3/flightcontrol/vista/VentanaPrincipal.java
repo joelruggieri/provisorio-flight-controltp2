c50760e3eb0e6e7c2003b6e37998ca828008383a
keynaka <keynaka@605bfb04-7f76-4739-ac2b-c6aeacdc3e22>
2012-12-12 00:31:41 +0000

    se separo a un paquete controlador lo que habia mesclado en la vista

--- src/fiuba/algo3/flightcontrol/vista/VentanaPrincipal.java
+++ src/fiuba/algo3/flightcontrol/vista/VentanaPrincipal.java
@@ -20,6 +20,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
+import fiuba.algo3.flightcontrol.controlador.Mouse;
 import fiuba.algo3.flightcontrol.modelo.Nivel;
 import fiuba.algo3.flightcontrol.modelo.ObjetoVolador;
 import fiuba.algo3.flightcontrol.modelo.Pista;
@@ -98,86 +99,10 @@ public class VentanaPrincipal {
 
 		this.setComponentsFocus(btnIniciar, btnDetener);
 
+		Mouse mouse = new Mouse(panel, unNivel);
+		mouse.agregarControlador();
 		
-				
-		panel.addMouseListener(new MouseAdapter() {
-				
-			private ObjetoVolador unAvion;
-			private List<Vector> unaLista;
-			JFrame frame;
-			
-			@Override
-			public void mouseClicked(MouseEvent click) {	
-				
-				JTextArea textArea = new JTextArea();
-				textArea.setLineWrap(true);
-				if (frame == null) {
-					frame = new JFrame("ATENCION");
-					frame.setBounds(50, 200, 200, 70);	
-				}
-
-				if (click.isAltDown()) {
-					if (this.unAvion == null) {
-					
-						this.unAvion = this.obtenerAvion(click);
-						if (this.unAvion != null) {
-							unaLista = new ArrayList<Vector>();
-							
-							textArea.setText("Agarro un Objeto Volador");
-							frame.getContentPane().add(textArea);
-							frame.setVisible(true);
-
-						}
-					
-					} else {
-						
-						Vector posicionClick = new Vector (click.getX(), click.getY());
-						unaLista.add(posicionClick);
-						
-						textArea.setText("Seteo una Posicion");
-						frame.getContentPane().add(textArea);
-						frame.setVisible(true);
-					}
-					
-				} else {
-						
-					if (this.unAvion != null) {
-						Trayectoria unaTrayectoria = new Trayectoria(unaLista);
-						this.unAvion.setTrayectoria(unaTrayectoria);
-						this.unAvion = null;
-						
-						textArea.setText("Seteo una nueva Trayectoria");
-						frame.getContentPane().add(textArea);
-						frame.setVisible(true);
-					}
-						
-				}
-					
-			}
-		
-
-			private ObjetoVolador obtenerAvion(MouseEvent click) {
-				boolean encontrado = false;
-				ObjetoVolador unAvion, avionEncontrado = null;
-				Vector posicionClick = new Vector (click.getX(), click.getY());
-				Vector posicionDeAvion;
-				double diferencia;
-				
-				Iterator<ObjetoVolador> it = unNivel.getObjetosVoladores();
-				while (it.hasNext() && !encontrado) {
-					unAvion = it.next();
-					posicionDeAvion = unAvion.getPosicion();
-					diferencia =  posicionClick.distancia(posicionDeAvion);
-					encontrado = (diferencia <= 20);
-					
-					if (encontrado){ 
-						avionEncontrado = unAvion;
-					}
-				}
-				
-				return avionEncontrado;
-			}});
-		}
+	}
 
 	private void inicializarModelo(SuperficieDeDibujo unPanel, Nivel unNivel) {
 		
